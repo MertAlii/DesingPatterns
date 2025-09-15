f41078f546e975d062c60c1543894ac3e412144d
MertAlii <Aslancocukmz@gmail.com>
2025-09-15 20:15:31 +0300

    Add project files.

---
+++ DesignPattern.ChainOfResponsibility/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.ChainOfResponsibility.DAL;
+
+public class Context: DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=MERTALII\\SQLEXPRESS;Database=DesingPattern;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+    }
+    public DbSet<CustomerProcess> CustomerProcesses { get; set; }
+}
---
+++ DesignPattern.ChainOfResponsibility/DAL/CustomerProcess.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPattern.ChainOfResponsibility.DAL;
+
+public class CustomerProcess
+{
+    public int CustomerProcessID { get; set; }
+    public string Name { get; set; } = null!;
+    public string Amount { get; set; } = null!;
+    public string EmployeeName { get; set; } = null!;
+    public string Description { get; set; } = null!;
+}
